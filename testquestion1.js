af9bdb4a555161bbfac50c59c6a9cc4b93051562
Theme <olliken17@gmail.com>
2025-08-27 08:22:59 +0000

    test

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,15 @@
+function setup() {
+  new Canvas(600,400);
+  background(150);
+  world.gravity.y = 5;
+}
 
+function draw() {
+   if(mouse.presses("left")) {
+    let clone = new Sprite (mouseX, mouseY);
+    clone.collider = "dynamic";
+   }
+}
+
+// you did not state that the sprite has to be a ball
+// there is a trail, idk why its there, but its not a requirement for it not to be there
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,19 @@
+function setup() {
+  new Canvas(600,400);
+  background(150);
+  world.gravity.y = 5;
 
+  let floor = new Sprite (300,385,600,30);
+  floor.collider = 'static';
+}
+
+function draw() {
+  if(mouse.presses("left")) {
+    let clone = new Sprite (mouseX, mouseY);
+    clone.collider = "dynamic";
+    clone.bounciness = 0.3;
+   }
+}
+
+// you did not state that the sprite has to be a ball
+// there is a trail, idk why its there, but its not a requirement for it not to be there
