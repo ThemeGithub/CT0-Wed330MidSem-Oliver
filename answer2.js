0f313de7da85c776b3838a480bd4036c66840d06
Theme <olliken17@gmail.com>
2025-09-03 07:43:40 +0000

    Sep 3, 2025, 7:43 AM

--- answer2.js
+++ answer2.js
@@ -1,7 +1,7 @@
 function setup() {
   new Canvas(600,400);
   world.gravity.y = 10;
-  floor = new Sprite(300, heighr=30, 600, 30)
+  floor = new Sprite(300, height-30, 600, 30, static)
 }
 
 function draw() {
