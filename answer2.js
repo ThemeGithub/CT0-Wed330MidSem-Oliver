5ad43b5be8e0ab7720f29d5427dbf53858c8f278
Theme <olliken17@gmail.com>
2025-09-03 07:43:20 +0000

    Sep 3, 2025, 7:43 AM

---
+++ answer2.js
@@ -0,0 +1,12 @@
+function setup() {
+  new Canvas(600,400);
+  world.gravity.y = 10;
+}
+
+function draw() {
+    background(220);
+   if(mouse.presses("left")) {
+    let clone = new Sprite (mouseX, mouseY, 30, 'dynamic');
+    noStroke();
+   }
+}
